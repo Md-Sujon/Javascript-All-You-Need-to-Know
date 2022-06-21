620c4376bc7c8644f45e45fc689a2b1677124816
Md-Sujon <mdsujon829562@gmail.com>
2022-06-21 18:25:27 +0600

    variable

---
+++ Variable.js
@@ -0,0 +1,7 @@
+var name = 'Sujon';
+var age = 24;
+var language = 'JavaScript'
+
+console.log("My name is",name);
+console.log("My Age",age);
+console.log("My favorite programming language",language);
