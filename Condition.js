5c973bbc5e66380083c28a70e248e33e2e2ea751
Md-Sujon <mdsujon829562@gmail.com>
2022-06-22 12:25:29 +0600

    condition

--- Condition.js
+++ Condition.js
@@ -1,1 +1,49 @@
-console.log('Sujon');
+var a = 1000;
+var b = 200;
+var c = 60;
+var d = 1500;
+// Find the Largest Among Three Numbers
+
+// use Math Function
+const largest = Math.max(a,b,c,d);
+const Largest = Math.max(a,b,c,d);
+console.log(largest);
+console.log(Largest);
+
+// use condition
+
+if(a>b && a>c){
+    console.log(a);
+}else if(b>a && b>c){
+    console.log(b);
+}else{
+    console.log(c);
+}
+
+
+// Find the Largest Among Four Numbers
+if(a>b && a>c && a>d){
+    console.log(a);
+}else if(b>a && b>c && b>d){
+console.log(b);
+}
+else if(c>a && c>b && d>c){
+     console.log(c);
+}else{
+    console.log(d);
+}
+
+
+
+// Find the Smalest Among Three Numbers use condition
+if(b>a && c>a){
+    console.log(a);
+}else if(a>b && c>b){
+    console.log(b);
+}else{
+    console.log(c);
+}
+
+
+
+
---
+++ TypeConversion.js
@@ -0,0 +1,17 @@
+// falsy value
+''
+0
+null
+undefined
+NaN
+0
+//others all value Truthy value
+452
+
+console.log(Boolean(''));
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(0));
+console.log(Boolean(452));
