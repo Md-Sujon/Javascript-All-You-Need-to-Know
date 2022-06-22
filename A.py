34b956443a1129f82f116256a6b3e23fca6d8599
Md-Sujon <mdsujon829562@gmail.com>
2022-06-22 13:28:44 +0600

    s

---
+++ A.py
@@ -0,0 +1,6 @@
+
+a=int(input('the numer which to be checked:'))
+if a%2==0:
+    print('numer is even:',(a))#if a got devided then it is even.
+else:
+    print('number is odd',(a))#if a left remainder it is odd.
--- Condition.js
+++ Condition.js
@@ -5,45 +5,49 @@ var d = 1500;
 // Find the Largest Among Three Numbers
 
 // use Math Function
-const largest = Math.max(a,b,c,d);
-const Largest = Math.max(a,b,c,d);
+/*
+const largest = Math.max(a, b, c, d);
+const Largest = Math.min(a, b, c, d);
 console.log(largest);
-console.log(Largest);
+console.log(Largest);*/
 
 // use condition
-
-if(a>b && a>c){
-    console.log(a);
-}else if(b>a && b>c){
-    console.log(b);
-}else{
-    console.log(c);
-}
-
+/*
+if (a > b && a > c) {
+  console.log(a);
+} else if (b > a && b > c) {
+  console.log(b);
+} else {
+  console.log(c);
+}*/
 
 // Find the Largest Among Four Numbers
-if(a>b && a>c && a>d){
-    console.log(a);
-}else if(b>a && b>c && b>d){
-console.log(b);
+/*
+if (a > b && a > c && a > d) {
+  console.log(a);
+} else if (b > a && b > c && b > d) {
+  console.log(b);
+} else if (c > a && c > b && d > c) {
+  console.log(c);
+} else {
+  console.log(d);
 }
-else if(c>a && c>b && d>c){
-     console.log(c);
-}else{
-    console.log(d);
-}
-
-
-
+*/
 // Find the Smalest Among Three Numbers use condition
-if(b>a && c>a){
-    console.log(a);
-}else if(a>b && c>b){
-    console.log(b);
+/*
+if (b > a && c > a) {
+  console.log(a);
+} else if (a > b && c > b) {
+  console.log(b);
+} else {
+  console.log(c);
+}*/
+
+
+// Even Odd number
+var E = 15;
+if(E % 2 == 0){
+    console.log('Even Number');
 }else{
-    console.log(c);
+    console.log('Odd Number');
 }
-
-
-
-
