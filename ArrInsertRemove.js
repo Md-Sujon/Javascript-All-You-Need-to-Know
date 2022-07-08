2c8f9db7d3a399e143263e55b927b1d5ce711943
Md-Sujon <mdsujon829562@gmail.com>
2022-07-08 09:01:53 +0600

    array

---
+++ ArrInsertRemove.js
@@ -0,0 +1,3 @@
+const arr = [5,87,9,6,5,1,2];
+arr.splice(3, 0,55,12,46,99);
+console.log(arr);
--- travers.js
+++ travers.js
@@ -4,3 +4,19 @@ for(var i=0; i<arr.length; i++){
     arr[i]=arr[i]+3;
 }
 console.log(arr);
+
+// Odd Even sum
+const arrs = [4,5,6,8,1,2,3];
+let result = 0;
+let results = 0;
+for(var i=0; i<arrs.length; i++){
+    if(arrs[i]%2===0){
+        result = result + arrs[i]
+    }else{
+        results = results + arrs[i]
+    }
+   
+}
+console.log(result);
+console.log(results);
+
